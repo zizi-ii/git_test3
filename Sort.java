007690c1590fa97fe80e0f78b76a6aa448944898
Lee, Jiwoo <zizii23027@gmail.com>
2025-04-16 10:18:03 +0900

    quick_sort 구현 완료

--- Sort.java
+++ Sort.java
@@ -1,15 +1,23 @@
 public class Sort {
 
     public static void sort(int[] a) {
-        for (int i = 1; i < a.length; ++i) {
-            int value = a[i];
-            int j;
-            for (j = i - 1; j >= 0; --j)
-                if (a[j] > value)
-                    a[j + 1] = a[j];
-                else
-                    break;
-            a[j + 1] = value;
-        }
+        quickSort(a, 0, a.length - 1);
+    }
+
+    static void swap(int[] a, int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+    static int partition(int[] a, int start, int end) {
+        // TODO: partition 알고리즘을 구현해야 함
+    }
+
+    static void quickSort(int[] a, int start, int end) {
+        if (start >= end) return;
+        int middle = partition(a, start, end);
+        quickSort(a, start, middle-1);
+        quickSort(a, middle+1, end);
     }
 }
